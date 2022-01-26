589f312f430270417b72cd9475e57be158c49752
Hasibul Hasan <mdhasibulctg100@gmail.com>
2022-01-26 22:10:42 +0600

    about page image added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,8 +11,8 @@ const Footer = () => {
                     <Link to="" class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
                         <img class='w-32 md:w-48 lg:w-56' src={logo} alt="" />
                     </Link>
-                    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 Tailblocks —
-                        <a href="https://google.com" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@knyttneve</a>
+                    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2022 HSB BLOGS —
+                        <a href="https://google.com" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@HasibulHasan</a>
                     </p>
                     <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                         <a href="https://google.com" class="text-gray-500">
