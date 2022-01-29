18356aa127c45575a5605ac46eb961b9eed0625e
Hasibul Hasan <mdhasibulctg100@gmail.com>
2022-01-29 23:49:50 +0600

    blog text change

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -14,4 +14,5 @@ const Projects = () => {
     );
 };
 
+
 export default Projects;
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -5,14 +5,14 @@ const Blog = () => {
     return (
         <div class="py-8 flex flex-wrap md:flex-nowrap">
             <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="font-semibold title-font text-gray-700">CATEGORY</span>
+                <span class="font-semibold title-font text-gray-700">Blog Category</span>
                 <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
             </div>
             <div class="md:flex-grow">
                 <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded mx-auto" alt="hero" src="https://dummyimage.com/720x600" />
-                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Bitters hashtag waistcoat fashion axe chia unicorn</h2>
-                <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-                <Link to="" class="text-indigo-500 inline-flex items-center mt-4">Learn More
+                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Lorem ipsum dolor sit amet.</h2>
+                <p class="leading-relaxed">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure accusamus accusantium obcaecati tempore odit fugit dicta quibusdam optio nihil magnam.</p>
+                <Link to="" class="text-indigo-500 inline-flex items-center mt-4">See Details
                     <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M5 12h14"></path>
                         <path d="M12 5l7 7-7 7"></path>
